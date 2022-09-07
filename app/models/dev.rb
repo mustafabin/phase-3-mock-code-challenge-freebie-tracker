38c57b8ad7c691616b9a32263a9e87df4610922d
Mustafa Binalhag <mbin828@gmail.com>
2022-09-07 11:45:45 -0400

    ar methods and associations

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
-
+    has_many :freebies
+    has_many :companies, through: :freebies
+    def received_one(item_name)
+        self.freebies.find_by(item_name: item_name) ? true :  false
+    end
+    def give_away(dev,freebie)
+        if self.id == freebie.dev.id
+            freebie.dev_id = dev.id
+            freebie.save
+            "Gave away my #{freebie.item_name} to #{dev.name}"
+        else
+            "I cant give away somthing that isnt mine"
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
 Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
@@ -17,5 +17,12 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create(item_name: "spinny hat", value:99, dev_id: rick.id, company_id: google.id)
+Freebie.create(item_name: "spinny hat", value:99, dev_id: morty.id, company_id: google.id)
+Freebie.create(item_name: "spinny hat", value:99, dev_id: morty.id, company_id: google.id)
+Freebie.create(item_name: "pants", value:35, dev_id: morty.id, company_id: facebook.id)
+Freebie.create(item_name: "shirt", value:19, dev_id: morty.id, company_id: enron.id)
+Freebie.create(item_name: "shoes", value:250, dev_id: morty.id, company_id: mifflin.id)
 
 puts "Seeding done!"
